c5a0b3a95acbd3b7f4eb1a40914c8a47f2acf324
Abdifatah Ibrahim <alsidiq099@gmail.com>
2022-07-22 08:51:44 +0300

    Q function

--- student/models.py
+++ student/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    classroom = models.IntegerField()
+    teacher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
--- student/views.py
+++ student/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from .models import Student, Teacher
+from django.db.models import Q
+from django.db import connection
 
-# Create your views here.
+# def student_list(request):
+#     posts = Student.objects.filter(classroom=4) & Student.objects.filter(first_name__startswith='Shikanda')
+
+#     return render(request, 'index.html', {'posts': posts})
+
+
+def student_list_(request):
+    posts = Student.objects.filter(Q(classroom=4) & Q(first_name__startswith='Shikanda'))
+
+    return render(request, 'index.html', {'posts': posts})
+
+
+def student_list_(request):
+    posts = Student.objects.all().values("first_name").union(
+        Teacher.objects.all().values_list('first_name'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'index.html', {'posts': posts})
+
+def student_list_(request):
+    posts = Student.objects.exclude(age=23) & Student.objects.exclude(first_name__startswith='Thomas')
+
+    
+
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'index.html', {'posts': posts})
+
+
+def student_list(request):
+    posts = Student.objects.filter(Q(age__gt=21) & Q(first_name__startswith='Thomas'))
+    
+
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'index.html', {'posts': posts})
